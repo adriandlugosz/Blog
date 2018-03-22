663afa5ad88b7167f3a9c565c6ff7543eaa7b581
Adrian Długosz <dlugosz.ad@gmail.com>
2018-03-22 22:29:50 +0100

    add data base

--- src/main/java/pl/adriandlugosy/BlogSpring/controller/BlogController.java
+++ src/main/java/pl/adriandlugosy/BlogSpring/controller/BlogController.java
@@ -3,7 +3,11 @@ package pl.adriandlugosy.BlogSpring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.adriandlugosy.BlogSpring.model.Response;
 import pl.adriandlugosy.BlogSpring.repository.BlogRepository;
 
 @Controller
@@ -14,42 +18,68 @@ public class BlogController {
     BlogRepository blogRepository;
 
     @GetMapping("/index")
-    public String index(){
+    public String index(ModelMap modelMap) {
+        modelMap.addAttribute("info", blogRepository.count());
         return "blog/index";
     }
 
     @GetMapping("/friends")
-    public String friends(){
+    public String friends(ModelMap modelMap) {
+        modelMap.addAttribute("info", blogRepository.count());
         return "blog/friends";
     }
 
     @GetMapping("/suits")
-    public String suits(){
+    public String suits(ModelMap modelMap) {
+        modelMap.addAttribute("info", blogRepository.count());
         return "blog/suits";
     }
 
     @GetMapping("/mother")
-    public String mother(){
+    public String mother(ModelMap modelMap) {
+       modelMap.addAttribute("info", blogRepository.count());
         return "blog/mother";
     }
 
     @GetMapping("/whitecollar")
-    public String whiteCollar(){
+    public String whiteCollar(ModelMap modelMap) {
+       modelMap.addAttribute("info", blogRepository.count());
         return "blog/whitecollar";
     }
 
     @GetMapping("/that70s")
-    public String that70s(){
+    public String that70s(ModelMap modelMap) {
+       modelMap.addAttribute("info", blogRepository.count());
         return "blog/that70s";
     }
 
     @GetMapping("/gameofthrones")
-    public String gameOfThrones(){
+    public String gameOfThrones(ModelMap modelMap) {
+       modelMap.addAttribute("info", blogRepository.count());
         return "blog/gameofthrones";
     }
 
     @GetMapping("/cooperation")
-    public String cooperation(){
+    public String cooperation(ModelMap modelMap) {
+       modelMap.addAttribute("info", blogRepository.count());
         return "blog/cooperation";
+    }
+
+    @GetMapping("/contact/form")
+    public String form(ModelMap modelMap) {
+        modelMap.addAttribute("response",new Response());
+       modelMap.addAttribute("info", blogRepository.count());
+        return "blog/contact";
+    }
+
+    @PostMapping("/contact")
+    public String data(@ModelAttribute Response response) {
+        blogRepository.save(response);
+        return "redirect:/contact";
+    }
+
+    @GetMapping("/contact")
+    public String contact(ModelMap modelMap) {
+        return "redirect:/contact/form";
     }
 }
